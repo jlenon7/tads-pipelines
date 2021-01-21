476b7d1713978767a4a316c2e0df6efca352e3e2
João Lenon <lenonsec7@gmail.com>
2021-01-21 12:51:56 -0300

    feat: Base Project

---
+++ tests/Utils/isCpf.test.ts
@@ -0,0 +1,26 @@
+import { isCpf } from '../../src/Utils/isCpf'
+
+describe('\n[UNIT] isCpf', () => {
+	it('Deve retornar true pois o CPF enviado é válido', () => {
+		// CPF Gerado utilizando https://4devs.com.br
+
+		expect(isCpf(52998224725)).toBe(true)
+		expect(isCpf('52998224725')).toBe(true)
+		expect(isCpf('529.982.247-25')).toBe(true)
+	})
+
+	it('Deve retornar false pois o CPF enviado é inválido', () => {
+		expect(isCpf(17512195051)).toBe(false)
+		expect(isCpf('17512195051')).toBe(false)
+		expect(isCpf('175.121.950-51')).toBe(false)
+
+		expect(isCpf(17512193051)).toBe(false)
+		expect(isCpf('17512193051')).toBe(false)
+		expect(isCpf('175.121.930-51')).toBe(false)
+	})
+
+	it('Deve retornar false pois o CPF enviado contém o valor 00000000000', () => {
+		expect(isCpf('00000000000')).toBe(false)
+		expect(isCpf('000.000.000-00')).toBe(false)
+	})
+})
